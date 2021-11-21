f10d9790cbb221e7613af7fd663a530026ff7777
rebixpl <rebixcraft@gmail.com>
2021-11-21 10:21:39 +0100

    finished app: added authentication bloc, splash screen, login bloc and some ui

---
+++ flutter_bloc/flutter_login/lib/login/view/login_form.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/login/login.dart';
+import 'package:formz/formz.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state.status.isSubmissionFailure) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              const SnackBar(
+                content: Text("Authentication Failure"),
+              ),
+            );
+        }
+      },
+      child: Align(
+        alignment: const Alignment(0, -1 / 3),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            _UsernameInput(),
+            Padding(padding: EdgeInsets.all(12)),
+            _PasswordInput(),
+            Padding(padding: EdgeInsets.all(12)),
+            _LoginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _UsernameInput extends StatelessWidget {
+  const _UsernameInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      buildWhen: (previous, current) => previous.username != current.username,
+      builder: (context, state) {
+        return TextField(
+          key: const Key('loginForm_usernameInput_textField'),
+          onChanged: (username) => context.read<LoginBloc>().add(
+                LoginUsernameChanged(username),
+              ),
+          decoration: InputDecoration(
+            labelText: "Username",
+            errorText: state.username.invalid ? "Invalid Username" : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  const _PasswordInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      buildWhen: (previous, current) => previous.password != current.password,
+      builder: (context, state) {
+        return TextField(
+          key: const Key('loginForm_passwordInput_textField'),
+          onChanged: (password) =>
+              context.read<LoginBloc>().add(LoginPasswordChanged(password)),
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            errorText: state.password.invalid ? 'Invalid Password' : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      buildWhen: (previous, current) => previous.status != current.status,
+      builder: (context, state) {
+        return state.status.isSubmissionInProgress
+            ? const CircularProgressIndicator()
+            : ElevatedButton(
+                key: const Key('loginForm_continue_raisedButton'),
+                child: const Text('Login'),
+                onPressed: state.status.isValidated
+                    ? () {
+                        context.read<LoginBloc>().add(const LoginSubmitted());
+                      }
+                    : null,
+              );
+      },
+    );
+  }
+}
