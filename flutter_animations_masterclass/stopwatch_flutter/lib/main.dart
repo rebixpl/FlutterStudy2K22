aee61f10206e6af91c3aa567b8c08cfe31939b3d
rebixpl <rebixcraft@gmail.com>
2021-12-21 16:13:05 +0100

    stopwatch app init

---
+++ flutter_animations_masterclass/stopwatch_flutter/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:stopwatch_flutter/ui/stopwatch.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Stopwatch(),
+      ),
+    );
+  }
+}
