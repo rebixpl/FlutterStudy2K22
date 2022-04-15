58a80f70f0e50114ca7ff7880ad55c96a39f2cb0
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-04-15 14:48:35 +0200

    step-2

--- flutter-state-management-course/statefulapp_course/lib/main.dart
+++ flutter-state-management-course/statefulapp_course/lib/main.dart
@@ -13,14 +13,33 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = "Tap the screen...";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: Text(title),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            title = DateTime.now().toIso8601String();
+          });
+        },
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
