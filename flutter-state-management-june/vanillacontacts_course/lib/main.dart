4e00f03a08fed9ea6997e30b8987faf50332661a
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-02 09:20:07 +0200

    state management basics #3
    new view > add contacts to contactsbook
    text editing controller
    define "/new-contact" as a new route
    navigating through pages

--- flutter-state-management-june/vanillacontacts_course/lib/main.dart
+++ flutter-state-management-june/vanillacontacts_course/lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/new-contact': (context) => const NewContactView(),
+      },
     );
   }
 }
@@ -31,9 +34,7 @@ class ContactBook {
   static final ContactBook _shared = ContactBook._sharedInstance();
   factory ContactBook() => _shared;
 
-  final List<Contact> _contacts = [
-    const Contact(name: "Foo Bar"),
-  ];
+  final List<Contact> _contacts = [];
 
   int get length => _contacts.length;
 
@@ -62,8 +63,60 @@ class HomePage extends StatelessWidget {
             return ListTile(title: Text(contact.name));
           }),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          await Navigator.of(context).pushNamed("/new-contact");
+        },
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({Key? key}) : super(key: key);
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a new contact"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: "Enter a new contact name here...",
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              final contact = Contact(name: _controller.text);
+              ContactBook().add(contact: contact);
+              Navigator.of(context).pop();
+            },
+            child: const Text("Add Contact"),
+          ),
+        ],
       ),
     );
   }
