2f860aebc1170bf972817d5b56dd090f2a807316
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-02 08:28:08 +0200

    state management basics #1

---
+++ flutter-state-management-june/vanillacontacts_course/android/app/src/main/kotlin/launchpad/app/vanillacontacts_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package launchpad.app.vanillacontacts_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter-state-management-june/vanillacontacts_course/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class Contact {
+  final String name;
+
+  const Contact({required this.name});
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+    );
+  }
+}
