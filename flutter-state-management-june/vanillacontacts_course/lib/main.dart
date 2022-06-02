d3b6a84aa048f21586b1232e124aa4066454264c
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-06-02 08:48:46 +0200

    state management basics #2
    contact book class that is a singleton
    home page body

--- flutter-state-management-june/vanillacontacts_course/lib/main.dart
+++ flutter-state-management-june/vanillacontacts_course/lib/main.dart
@@ -26,14 +26,44 @@ class Contact {
   const Contact({required this.name});
 }
 
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = [
+    const Contact(name: "Foo Bar"),
+  ];
+
+  int get length => _contacts.length;
+
+  void add({required Contact contact}) => _contacts.add(contact);
+
+  void remove({required Contact contact}) => _contacts.remove(contact);
+
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
+      ),
+      body: ListView.builder(
+          itemCount: contactBook.length,
+          itemBuilder: (context, i) {
+            final contact = contactBook.contact(atIndex: i)!;
+            return ListTile(title: Text(contact.name));
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
